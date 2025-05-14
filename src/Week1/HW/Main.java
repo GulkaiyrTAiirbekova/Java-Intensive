16b60315ccd0d9d6e26ab9faf1b81a6176ed6a17
Your Name <taiirbekovag@gmail.com>
2025-05-14 11:47:37 +0600

    Switch method

---
+++ src/Week1/HW/Main.java
@@ -0,0 +1,37 @@
+package Week1.HW;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc =  new Scanner(System.in);
+        int dayNumber = sc.nextInt();
+
+        switch (dayNumber){
+            case 1:
+                System.out.println("Monday - Start of the work week.");
+                break;
+            case 2:
+                 System.out.println("Tuesday -Keep going!");
+                 break;
+            case 3:
+                System.out.println("Wednesday -  Midweek already");
+                break;
+            case 4:
+                System.out.println("Tuesday- Almost there");
+                break;
+            case 5:
+                System.out.println("Friday - Weekend is near!");
+                break;
+            case 6:
+                System.out.println("Saturday -  Time to relax!");
+                break;
+            case 7:
+                System.out.println("Sunday-  Rest and Recharge!");
+                break;
+            default:
+                System.out.println("Invalid day number");
+
+        }
+    }
+}
