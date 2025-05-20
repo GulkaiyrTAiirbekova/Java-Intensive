f5026791c7d2c5288ae6edefd28befd7028cf084
Your Name <taiirbekovag@gmail.com>
2025-05-20 00:50:30 -1200

    Local Date Time

--- src/Week1/Main.java
+++ src/Week1/Main.java
@@ -107,11 +107,11 @@ import java.time.LocalDate;  // import the LocalDate class
 //    }
 //}
 
-import java.time.LocalTime;  // import the LocalTime class
-
-public class Main {
-    public static void main(String[] args) {
-        LocalTime myObj = LocalTime.now();
-        System.out.println(myObj);
-    }
-}
+//import java.time.LocalTime;  // import the LocalTime class
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        LocalTime myObj = LocalTime.now();
+//        System.out.println(myObj);
+//    }
+//}
