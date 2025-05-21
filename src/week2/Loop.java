4788e1483a7aa77c46eb45224b236f02fb9d4f06
Your Name <taiirbekovag@gmail.com>
2025-05-21 22:04:20 +0600

    some errors , this code can be run only in Main but not in Random , need to clarify

--- src/week2/Loop.java
+++ src/week2/Loop.java
@@ -8,28 +8,28 @@ package week2;
 //        }
 //    }
 //}
-
-public class Loop  {
-    public static void main(String [] args){
-        String [] months = new String[]{
-                "January",
-                "February",
-                "March",
-                "April",
-                "May",
-                "June",
-                "July",
-                "August",
-                "September",
-                "October",
-                "November",
-                "December"
-        };
-//        for (int i = 0; i <months.length; i++){
+//SMTH IS WRONG HERE
+//public class Loop{
+//    public static void main(String [] args){
+//        String [] months = new String[]{
+//                "January",
+//                "February",
+//                "March",
+//                "April",
+//                "May",
+//                "June",
+//                "July",
+//                "August",
+//                "September",
+//                "October",
+//                "November",
+//                "December"
+//        };
+////        for (int i = 0; i <months.length; i++){
+////            System.out.println(months[i]);
+//        //FOR REVERSE
+//        for (int i = months.length-1; i >=0;i++){
 //            System.out.println(months[i]);
-        //FOR REVERSE
-        for (int i = months.length-1; i >=0;i++){
-            System.out.println(months[i]);
-        }
-    }
-}
+//        }
+//    }
+//}
--- src/week2/Main.java
+++ src/week2/Main.java
@@ -1,4 +1,28 @@
 package week2;
 
+import java.util.Scanner;
+import java.util.Random;
+
+
 public class Main {
+    public static void main (String [] args){
+        Random rand = new Random();
+        int number = Math.abs(rand.nextInt()) %100;
+        //System.out.println(rand.nextInt());
+        System.out.println(number);
+        Scanner sc = new Scanner (System.in);
+//        int guess = sc.nextInt();
+        int guess = -1;
+        while (guess != number){
+            if(number == guess){
+                System.out.println("You win!");
+            } else{
+                if(guess < number){
+                    System.out.println(" less");
+                } else {
+                    System.out.println("greater");
+                }
+            }
+        }
+    }
 }
