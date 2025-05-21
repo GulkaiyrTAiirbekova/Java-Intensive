fdd9a2a2bf98ab089fcc6028ca429d316e7285aa
Your Name <taiirbekovag@gmail.com>
2025-05-22 04:15:10 +0600

    week9

---
+++ src/Week9/Human.java
@@ -0,0 +1,11 @@
+package Week9;
+
+public class Human {
+    private int age;
+
+    @override
+    public String toString(){
+        return "Human{" +
+                "age=" + age+
+    }
+}
---
+++ src/Week9/Person.java
@@ -0,0 +1,12 @@
+package Week9;
+
+public class Person extends Human Implements Serilizable {
+    private String name;
+    private String adresses;
+    private List Pet
+
+    public Person(int age,String name,String Adresses){
+        super(age);
+
+    }
+}
