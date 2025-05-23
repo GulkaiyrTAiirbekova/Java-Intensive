2722f9293a653e277319fc18ad1e5619bb7afba5
Your Name <taiirbekovag@gmail.com>
2025-05-23 21:45:43 +0600

    String Iteration example

--- src/week3/StringIterations.java
+++ src/week3/StringIterations.java
@@ -1,13 +1,20 @@
 package week3;
-
-import java.util.Locale;
-
+//
+//import java.util.Locale;
+//
+//public class StringIterations {
+//    public static void main (String [] args){
+//        //int, double
+//        //int[]
+//        String s = "string";
+//        //System.out.println(s);
+//        //System.out.println(s.length());
+//        for (int i = 0; i< s.length(); ++i){
+//    }
+//}
 public class StringIterations {
-    public static void main (String [] args){
-        //int, double
-        //int[]
-        String s = "string";
-        System.out.println(s);
-        System.out.println(s.length());
+    public static void main(String[] args) {
+        String s = "Hello";
+        System.out.println(s.length()); // Correct spelling
     }
 }
