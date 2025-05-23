656eb8b4fc2d147bdebef71b008f6715336c69b7
Your Name <taiirbekovag@gmail.com>
2025-05-23 21:22:42 +0600

    String Iteration

---
+++ src/week3/StringIterations.java
@@ -0,0 +1,13 @@
+package week3;
+
+import java.util.Locale;
+
+public class StringIterations {
+    public static void main (String [] args){
+        //int, double
+        //int[]
+        String s = "string";
+        System.out.println(s);
+        System.out.println(s.length());
+    }
+}
