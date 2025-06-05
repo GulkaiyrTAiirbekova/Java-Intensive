7f9743d90fa308e8434bf5be7d3c9f3461ec7e2c
Your Name <taiirbekovag@gmail.com>
2025-06-05 06:15:54 +0600

    GenerateBitmasks

--- src/methods/Factorial.java
+++ src/methods/Factorial.java
@@ -1,17 +1,31 @@
 package methods;
 //Recursion = func that calls itself
+//public class Factorial {
+//   // n! = 1 *2 *3 *...n
+//    //factorial(n) = |factorial(n-1) * n,if n >1
+//    // | 1,if n == 1
+//    public static int factorial(int n){
+//        if (n== 1){
+//            return 1;
+//        }else{
+//            return factorial(n - 1) * n;
+//        }
+//    }
+//    public static void main(String [] ars){
+//        System.out.println(factorial(5));
+//    }
+//}
 public class Factorial {
-   // n! = 1 *2 *3 *...n
-    //factorial(n) = |factorial(n-1) * n,if n >1
-    // | 1,if n == 1
     public static int factorial(int n){
-        if (n== 1){
+        if (n == 1){
             return 1;
-        }else{
-            return factorial(n - 1) * n;
+        } else{
+            return factorial(n-1) *n;
         }
     }
-    public static void main(String [] ars){
-        System.out.println(factorial(5));
-    }
+//    public static int factorialWithLoops(int n){
+//        int result =1;
+//        for (int i=1; i<=n; ++i);
+//        result = result * i;
+//    } return result;
 }
---
+++ src/methods/GenerateBitmasks.java
@@ -0,0 +1,37 @@
+package methods;
+
+import java.util.Scanner;
+
+//n =2
+// 00
+//11
+//01
+//11
+//n=3
+//000
+//001
+//111
+public class GenerateBitmasks {
+    //ouTPUT:
+    //0
+    //n=3
+    //n=2
+    public static void generate(int n, String current){
+        if (n==0){
+            System.out.println(current);
+            return;
+        }
+        current += "0";
+        generate(n-1,current);
+        current = current.substring(0,current.length()-1);
+        current +="1";
+        generate(n-1,current);
+        current = current.substring(0,current.length()-1);
+    }
+    public static void main(String [] args){
+        Scanner sc =  new Scanner(System.in);
+        int n = sc.nextInt();
+        generate(n,"");
+
+    }
+}
