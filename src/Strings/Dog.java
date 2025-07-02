e070e9f10067a0b6faa25870a08124dc0baf1c0b
Your Name <taiirbekovag@gmail.com>
2025-07-02 11:37:41 +0600

    Strings

---
+++ src/Strings/Dog.java
@@ -0,0 +1,24 @@
+//package Strings;
+//
+//public class Dog {
+//    private String name;
+//    private String breed;
+//
+//    //Constructor
+//    public Dog("Money","Labrador"){
+//        //Initialize attributes
+//        this.name = name;
+//        this.breed = breed;
+//    }
+//    //method barked
+//    public void bark(){
+//        System.out.println(name + "barked");
+//        //Additional getter for getting the name of the dog
+//    public String getName(){
+//         return name;
+//        }
+//    public String getBreed(){
+//        return breed;
+//        }
+//    }
+//}
--- src/Week4/Main.java
+++ src/Week4/Main.java
@@ -1,15 +1,15 @@
-//package Week4;
-//
-//public class Main {
-//    public static void main(String[] args){
-//        Dog bobby = new Dog("Bobby");
-//
-//        bobby.name = "Bobby";
-//
-//        bobby.bark();
-//        Dog buddy = new Dog();
-//        buddy.name = "Buddy";
-//        buddy.bark();
-//
-//    }
-//}
+package Week4;
+//class has a method and  attribute of the class
+//constructor can change the behavior
+public class Main {
+    public static void main(String [] args){
+        Dog bobby = new Dog();
+        bobby.name = "Money";
+
+        bobby.bark();
+        Dog buddy = new Dog();
+        buddy.name = "Buddy";
+        buddy.bark();
+    }
+}
+
