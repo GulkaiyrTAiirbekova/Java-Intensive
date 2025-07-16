7367686f3f64b8e91aab57ebfdf87f4c277aae00
Your Name <taiirbekovag@gmail.com>
2025-07-16 23:24:18 +0600

    Brute Force,Hash Set, hash Set Length, Sorting

--- src/neetcode/ContainsDuplicate_217.java
+++ src/neetcode/ContainsDuplicate_217.java
@@ -26,3 +26,55 @@ public class hasDuplicate {
         return false;
     }
 }
+
+
+//2. Sorting
+
+"""
+Time & Space Complexity
+Time complexity: O(nlogn)
+Space complexity:O(1) or O(n) depending on the sorting algorithm.
+"""
+public class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+//3. Hash Set
+
+"""
+Time & Space Complexity
+Time complexity: O(n)
+Space complexity:O(n)
+"""
+public class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
+            }
+            seen.add(num);
+        }
+        return false;
+    }
+}
+//3. Hash Set Length
+
+"""
+Time & Space Complexity
+Time complexity: O(n)
+Space complexity:O(n)
+"""
+public class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        return Arrays.stream(nums).distinct().count() < nums.length;
+    }
+}
