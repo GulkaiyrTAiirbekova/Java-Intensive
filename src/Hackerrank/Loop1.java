c15d57e45c1f25f824be8c391ace8508ea22b5df
Gulkaiyr Taiirbekova <taiirbekovag@gmail.com>
2025-12-10 02:52:20 +0600

    Add Loop1 solution from Hackerrank

---
+++ src/Hackerrank/Loop1.java
@@ -0,0 +1,15 @@
+package Hackerrank;
+
+import java.util.Scanner;
+
+public class Loop1 {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for(int i =1; i <= 10;i++){
+            System.out.println(N + " x " + i + " N * i");
+        }
+    }
+}
