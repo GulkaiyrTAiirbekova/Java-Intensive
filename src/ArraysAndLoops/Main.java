72825888fa1fbb13676d94292277455f395fb2c0
Gulkaiyr Taiirbekova <taiirbekovag@gmail.com>
2025-12-24 18:50:04 -1100

    feat(learning): Implement array initialization and indexing basics
    -Instantiate fixed-sized integer array with 100 slots;
    -Demonstrate manual elements assignment via index.
    - Include print statements for verification of memory addresses/values.
    -Comment out double array shorthand for future reference.

---
+++ src/ArraysAndLoops/Main.java
@@ -0,0 +1,19 @@
+package ArraysAndLoops;
+
+public class Main {
+    public static void main(String [] args){
+        int a,b,c,d,e;
+        int [] array = new int [100];
+        array[0] = 10;
+        array[1] = 20;
+        array[2] = 30;
+
+
+        //another syntax
+//        double [] array2 = new double []{40,50,60};
+
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+//        System.out.println(array2[2]);
+    }
+}
