adc978b8a454c5b4876735984254f44c3246a80f
Gulkaiyr Taiirbekova <taiirbekovag@gmail.com>
2025-12-27 21:44:40 -1000

    Add basic counter-based for loop example

---
+++ src/Loop/ForLoop.java
@@ -0,0 +1,13 @@
+package Loop;
+
+
+public class ForLoop {
+    public static void main(String []args){
+        for (int i = 0; i < 5; i++){
+            System.out.println(i);
+        }
+    }
+}
+
+//Repeats a block of code with counter
+//Common in counting tasks
