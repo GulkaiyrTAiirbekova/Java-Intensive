55740cb9ecd0474e924de9ffe227cfe2710f001c
Gulkaiyr Taiirbekova <taiirbekovag@gmail.com>
2025-12-28 14:23:53 +0600

    Fix compilation error by declaring Month[] before indexed iteration

--- src/Loop/IterateMonths.java
+++ src/Loop/IterateMonths.java
@@ -4,16 +4,24 @@ import java.time.Month;
 import java.time.format.TextStyle;
 import java.util.Locale;
 
+
 public class IterateMonths {
     public static void main(String [] args){
-        for (Month month : Month.values()){
+        Month [] months = Month.values();
+
+        for (Month month : months){
+
            //To get the full name of the month("January"), abbreviated ("Jan"), etc.
            String fullName = month.getDisplayName(TextStyle.FULL,Locale.ENGLISH);
            int monthValue = month.getValue();//1 for January and etc
             int dayInMonth = month.maxLength();//Max possible days(29 for Feb)
 
-            System.out.println(fullName + " is month number " + monthValue +" and has up to " + dayInMonth + " days ." );
-        }
 
+            System.out.println(fullName + " is month " + monthValue +
+                    " and has up to " + dayInMonth + " days");
+        }
+        for (int i = 0; i < months.length; i+=2 ){
+            System.out.println("Index" + i + " , Month: " + months[i]);
+        }
     }
 }
