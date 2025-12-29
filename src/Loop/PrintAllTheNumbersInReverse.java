121a6db06d7509a7951a6e02d89937f465de9aa1
Gulkaiyr Taiirbekova <taiirbekovag@gmail.com>
2025-12-29 16:38:21 +0600

    Start PrintAllTheNumbersInReverse: initialize array to store user input

---
+++ src/Loop/PrintAllTheNumbersInReverse.java
@@ -0,0 +1,12 @@
+package Loop;
+
+import java.util.Scanner;
+
+public class PrintAllTheNumbersInReverse {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] array = new int[n];
+    }
+}
