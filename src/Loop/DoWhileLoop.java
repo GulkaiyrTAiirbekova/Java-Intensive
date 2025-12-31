c9534ec21c66668539f849515104d7914ad06bba
Gulkaiyr Taiirbekova <taiirbekovag@gmail.com>
2025-12-31 20:56:30 -0300

    Fix undefined target number and missing Scanner import in guessing game

---
+++ src/Loop/DoWhileLoop.java
@@ -0,0 +1,12 @@
+package Loop;
+
+public class DoWhileLoop {
+    public static void main(String [] args){
+        int i = 0;
+        do{
+            System.out.println(i);
+            i++;
+        } while ( i < 9 );
+    }
+}
+//Execute given block at least once
