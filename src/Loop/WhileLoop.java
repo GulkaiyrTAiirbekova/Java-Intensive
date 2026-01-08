a011542c8056a006383e9d69ec4be70e52f2dec7
Gulkaiyr Taiirbekova <taiirbekovag@gmail.com>
2026-01-07 18:56:19 -1200

    Add basic while-loop example illustrating conditional iteration

---
+++ src/Loop/WhileLoop.java
@@ -0,0 +1,13 @@
+package Loop;
+
+public class WhileLoop {
+    public static void main(String [] args){
+        int i =0;
+        while(i < 5){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+//executes as long as a condition is true
+//use when number of iteration is not fixed
