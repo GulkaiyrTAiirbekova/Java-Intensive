82527909cf86cfd889148663d99abe2e470cb02b
Gulkaiyr Taiirbekova <taiirbekovag@gmail.com>
2026-01-12 21:21:02 -1100

    Implement SingleIteration

---
+++ src/StringMethods/StringIteration.java
@@ -0,0 +1,9 @@
+package StringMethods;
+
+public class StringIteration {
+    public static void main(String [] args){
+        String s = "string";
+        System.out.println(s);
+    }
+}
+//Add basic string initialization and output example
